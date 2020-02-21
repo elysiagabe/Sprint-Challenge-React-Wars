359b11850de1de48a990ab70a077c88a37c2d611
Elysia Gabe <elysiagabe@gmail.com>
2020-02-21 08:36:04 -0800

    Sets up component structure and renders app w/ temporarily hard-coded data

---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CharacterCard = () => {
+    return (
+        <div>
+            <h2>Name: LUKE</h2>
+            <p>Gender: MALE</p>
+            <p>Birth Year: YEAR</p>
+            <p>Species: HUMAN</p>
+            <p>Homeworld: PLANET</p>
+        </div>
+    );
+}
+
+export default CharacterCard;
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CharacterCard from "./CharacterCard";
+
+const CharacterList = () => {
+
+    // set state
+
+    // axios call & useEffect hook
+
+    return (
+        <section>
+            {/* Map thru data here and set up card for each character from API */}
+            <CharacterCard />
+        </section>
+    );
+}
+
+export default CharacterList;
